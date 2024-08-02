bb30c7f2a0c633c4254eaa16fedea11dbcf74558
tdance555 <2024tdance@gmail.com>
2024-08-02 19:59:09 +0800

    Add files via upload

---
+++ mysite/trips/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+    number = models.PositiveIntegerField(unique=True)
+    batch = models.CharField(max_length=100)  # 梯次 第一天,第二天
+    route = models.CharField(max_length=1)  #路線名稱 A,B,C
+    title = models.CharField(max_length=100) #廠商名稱
+    icon = models.URLField(blank=True) #廠商icon
+    question = models.CharField(max_length=100) #題目
+    choiceA = models.CharField(max_length=100) #選項
+    choiceB = models.CharField(max_length=100)
+    choiceC = models.CharField(max_length=100)
+    choiceD = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100) #答案
+
+    def __str__(self):
+        return f"{self.number}. {self.question}"
+    
+# class Post1(models.Model):
+#     number = models.PositiveIntegerField()
+#     title = models.CharField(max_length=100)
+#     content = models.TextField(blank=True)
+#     image = models.ImageField(upload_to='questions/', blank=True, null=True)
+
+#     def __str__(self):
+#         return f"{self.number}. {self.title}"
