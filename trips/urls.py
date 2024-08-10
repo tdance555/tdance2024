96786632ba9a5969334b231b45a03ab096cc8c1c
Benjamin Shih <shih.zhi.bin@gmail.com>
2024-08-10 10:23:15 +0800

    三條路線跟6間廠商

--- trips/urls.py
+++ trips/urls.py
@@ -5,10 +5,13 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', PostListCreate.as_view(), name='post-list-create'),
-    path('profile/', views.user_profile, name='user_profile'),
-    path('profile/<int:number>/', views.profile_success, name='profile_success'),
-    # 其他路由配置
-    path('templates/ar_scan01/', views.ar_scan01, name='ar_scan01'),
+    path('profile/', views.user_profile, name='user_profile'),  # Profile page URL (e.g., data entry)
+    path('profile/manual/', views.manual, name='manual'),  # Manual page URL
+    path('profile/manual/road/', views.road, name='road'),  # Road page URL
+    path('profile/manual/road/route1/', views.route1, name='route1'),  # 添加这一行
+    path('profile/manual/road/route2/', views.route2, name='route2'),  # 添加这一行
+    path('profile/manual/road/route3/', views.route3, name='route3'),  # 添加这一行
+    path('profile/manual/road/<int:number>/', views.profile_success, name='profile_success'),
 
 ]
 
--- trips/views.py
+++ trips/views.py
@@ -15,21 +15,38 @@ class PostListCreate(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 # Create your views here.
+
 def user_profile(request):
     if request.method == 'POST':
         form = UserProfileForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('profile_success')  # 成功後重定向到成功頁面
+            # 表单成功后重定向到 manual 页面
+            return redirect('manual')
     else:
         form = UserProfileForm()
 
     return render(request, 'user_profile.html', {'form': form})
 
-def profile_success(request,number):
-    question = get_object_or_404(Question,  number=number)
 
+def profile_success(request, number):
+    question = get_object_or_404(Question, number=number)
     return render(request, 'profile_success.html', {'question': question})
 
 def ar_scan01(request):
     return render(request, 'ar_scan01.html')
+
+def manual(request):
+    return render(request, 'manual.html')
+
+def road(request):
+    return render(request, 'road.html')
+
+def route1(request):
+    return render(request, 'route1.html')
+
+def route2(request):
+    return render(request, 'route2.html')
+
+def route3(request):
+    return render(request, 'route3.html')
