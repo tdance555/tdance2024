e487fec8769d2378720c0b44266c6b8e7292c6f0
anny114 <100180901+anny114@users.noreply.github.com>
2024-08-21 11:47:45 +0800

    換背景
    換背景

--- trips/urls.py
+++ trips/urls.py
@@ -11,14 +11,33 @@ urlpatterns = [
     path('profile/manual/function/road/route1/', views.route1, name='route1'),  # 添加这一行
     path('profile/manual/function/road/route2/', views.route2, name='route2'),  # 添加这一行
     path('profile/manual/function/road/route3/', views.route3, name='route3'),  # 添加这一行
-    # AR頁面------------------------------------------------------------------------------------
+    # AR頁面---------------------------------------------------------------------------------------------------
+    # 第一梯次
     path('profile/manual/function/road/route1/arScan1', views.arScan1, name='arScan1'),
     path('profile/manual/function/road/route1/arScan2', views.arScan2, name='arScan2'),
     path('profile/manual/function/road/route1/arScan3', views.arScan3, name='arScan3'),
     path('profile/manual/function/road/route1/arScan4', views.arScan4, name='arScan4'),
     path('profile/manual/function/road/route1/arScan5', views.arScan5, name='arScan5'),
     path('profile/manual/function/road/route1/arScan18', views.arScan18, name='arScan18'),
+
+    path('profile/manual/function/road/route2/arScan6', views.arScan6, name='arScan6'),
+    path('profile/manual/function/road/route2/arScan7', views.arScan7, name='arScan7'),
+    path('profile/manual/function/road/route2/arScan8', views.arScan8, name='arScan8'),
+    path('profile/manual/function/road/route2/arScan9', views.arScan9, name='arScan9'),
+    path('profile/manual/function/road/route2/arScan10', views.arScan10, name='arScan10'),
+    path('profile/manual/function/road/route2/arScan11', views.arScan11, name='arScan11'),
+
+    path('profile/manual/function/road/route3/arScan12', views.arScan12, name='arScan12'),
+    path('profile/manual/function/road/route3/arScan13', views.arScan13, name='arScan13'),
+    path('profile/manual/function/road/route3/arScan14', views.arScan14, name='arScan14'),
+    path('profile/manual/function/road/route3/arScan15', views.arScan15, name='arScan15'),
+    path('profile/manual/function/road/route3/arScan16', views.arScan16, name='arScan16'),
+    path('profile/manual/function/road/route3/arScan17', views.arScan17, name='arScan17'),
+
+    # 第二梯次
     
+
+    # API調用----------------------------------------------------------------------------------------------------
     # path('get_api/', views.get_api, name='get_api'),
     path('api/user/', UserProfileAPIView.as_view(), name='user-create'),
     path('api/question/<int:question_id>/', QuestionDetailAPIView.as_view(), name='question-detail'),
